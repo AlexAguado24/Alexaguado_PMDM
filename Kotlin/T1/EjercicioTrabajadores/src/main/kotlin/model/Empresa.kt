43a497ab9fc3875e0c663c0fea29d8786cd7ba64
AlexAguado24 <aaguado131@gmail.com>
2022-09-30 11:07:00 +0200

    .

--- Kotlin/T1/EjercicioTrabajadores/src/main/kotlin/model/Empresa.kt
+++ Kotlin/T1/EjercicioTrabajadores/src/main/kotlin/model/Empresa.kt
@@ -4,6 +4,7 @@ import java.util.ArrayList
 class Empresa {
 
     var listaTrabajadores: ArrayList<Trabajador>?;
+    var trabajadorADespedir: Trabajador? = null
     init {
         listaTrabajadores = ArrayList()
     }
@@ -72,35 +73,15 @@ class Empresa {
             println("El trabajador indicado no existe")
         }
     }
-
-    /*fun despedirTrabajador(dniJefe: String, dniTrabajador: String) {
-        var existe: Boolean = false
-        var existeJefe : Boolean = false
-        listaTrabajadores!!.forEach { item ->
-            if (item.getDNI() == dniJefe) {
-                existeJefe = true
-                listaTrabajadores!!.forEachIndexed{
-                    index, trabajador ->
-                }
-            }
-        }
-        if (trabajador.getDNI() == (dniTrabajador)){
-            listaTrabajadores!!.remove(trabajador)
-        }
-        if (!existe || !existeJefe) {
-            println("El trabajador o jefe que buscas no estan en la empresa")
-        }
-
-    }*/
-    fun despedirTrabajador(dni: String, trabajadorADespedir: Trabajador) {
-
+    fun despedirTrabajador(dni: String) {
         listaTrabajadores!!.forEach { persona ->
             if (dni.equals(persona.getDNI()) && persona is Jefe) {
                 var dni_empleado: String? = null
                 println("Introduzca DNI del trabajador:")
                 dni_empleado = readLine()?.toString()
                 listaTrabajadores!!.forEach { trabajador ->
-                    if (dni_empleado == trabajadorADespedir.getDNI()) {
+                    if (dni_empleado == trabajador.getDNI()) {
+                        trabajadorADespedir = trabajador
                         println("Trabajador despedido")
                     } else {
                         println("El trabajador no encontrado")
