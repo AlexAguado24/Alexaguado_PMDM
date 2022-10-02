da8f2b843dba829c190db128cc7dbf239b5941ab
AlexAguado24 <aaguado131@gmail.com>
2022-10-02 21:53:25 +0200

    .

--- Kotlin/T1/EjercicioLlamadas/src/main/kotlin/MainLlamadas.kt
+++ Kotlin/T1/EjercicioLlamadas/src/main/kotlin/MainLlamadas.kt
@@ -7,17 +7,27 @@ fun main() {
 
     var centralita: Centralita? = Centralita()
 
-    var provincial: LlamadaProvincial = LlamadaProvincial(1123,412,30)
+    var provincial1: LlamadaProvincial = LlamadaProvincial(1123,412,30)
+    var provincial2: LlamadaProvincial = LlamadaProvincial(2345,124,15)
+    var provincial3: LlamadaProvincial = LlamadaProvincial(3456,365,21)
+
     var nacional1: LlamadaNacional = LlamadaNacional(456,457,20,1)
     var nacional2: LlamadaNacional = LlamadaNacional(567,568,20,2)
     var nacional3: LlamadaNacional = LlamadaNacional(678,789,20,3)
-    var local: LlamadaLocal = LlamadaLocal(821,218,10)
 
-    centralita!!.registrarLlamada(local)
+    var local1: LlamadaLocal = LlamadaLocal(821,218,10)
+    var local2: LlamadaLocal = LlamadaLocal(912,245,20)
+    var local3: LlamadaLocal = LlamadaLocal(934,321,30)
+
+    centralita!!.registrarLlamada(local1)
+    centralita!!.registrarLlamada(local2)
+    centralita!!.registrarLlamada(local3)
     centralita!!.registrarLlamada(nacional1)
     centralita!!.registrarLlamada(nacional2)
     centralita!!.registrarLlamada(nacional3)
-    centralita!!.registrarLlamada(provincial)
+    centralita!!.registrarLlamada(provincial1)
+    centralita!!.registrarLlamada(provincial2)
+    centralita!!.registrarLlamada(provincial3)
 
 
     centralita!!.mostrarLlamadas("todas")
