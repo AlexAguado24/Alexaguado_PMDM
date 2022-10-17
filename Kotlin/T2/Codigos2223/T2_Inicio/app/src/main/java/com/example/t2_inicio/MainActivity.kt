95903d0af060ac4cf77a952ae34ae93902bb1d73
AlexAguado24 <aaguado131@gmail.com>
2022-10-17 20:58:05 +0200

    .

--- Kotlin/T2/Codigos2223/T2_Inicio/app/src/main/java/com/example/t2_inicio/MainActivity.kt
+++ Kotlin/T2/Codigos2223/T2_Inicio/app/src/main/java/com/example/t2_inicio/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.t2_inicio.R
 
 class MainActivity : AppCompatActivity() {
 
