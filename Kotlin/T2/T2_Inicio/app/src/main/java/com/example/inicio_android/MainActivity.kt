2a15f1cff2e3afa22c6ea933588d6e4c223db121
AlexAguado24 <aaguado131@gmail.com>
2022-10-18 21:09:12 +0200

    .

--- Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
+++ Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.inicio_android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var botonPulsar: Button;
     lateinit var textoSaludo: TextView;
+    lateinit var editNombre: EditText;
 
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,10 +24,16 @@ class MainActivity : AppCompatActivity() {
         Log.v("ciclo_vida", "Metodo onCreate ejecutado")
         botonPulsar = findViewById(R.id.boton_pulsar)
         textoSaludo = findViewById(R.id.texto_bienvenida)
+        editNombre = findViewById(R.id.edit_nombre)
 
-        botonPulsar.setOnClickListener({
-            Log.v("ciclo_vida", "Boton pulsado")
-            textoSaludo.setText("Primera app completa")
+        botonPulsar.setOnClickListener({view: View ->
+            if (!editNombre.text.isEmpty()) {
+                var texto = editNombre.text
+                textoSaludo.setText(texto)
+                editNombre.setText("")
+            } else {
+                Log.v("avisos","texto vacio")
+            }
         })
     }
 
@@ -40,16 +50,6 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.v("ciclo_vida","onPause ejecutado")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v("ciclo_vida","onStop ejecutado")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v("ciclo_vida","onDestroy ejecutado")
     }
 
 }
