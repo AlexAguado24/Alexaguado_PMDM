0c9e5175297f8ab957242e447c261fb6ef5da52f
AlexAguado24 <aaguado131@gmail.com>
2022-10-18 19:54:10 +0200

    .

---
+++ Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.inicio_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    
+    lateinit var botonPulsar: Button;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // xml + kt
+
+
+    }
+
+
+}
