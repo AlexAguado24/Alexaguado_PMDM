82d06cd9388d8e2354410f4613263cf9cca5d0e0
AlexAguado24 <aaguado131@gmail.com>
2022-10-18 20:34:29 +0200

    .

--- Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
+++ Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.inicio_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    
+
     lateinit var botonPulsar: Button;
+    lateinit var textoSaludo: TextView;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +17,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // xml + kt
+        Log.v("ciclo_vida", "Metodo onCreate ejecutado")
+        botonPulsar = findViewById(R.id.boton_pulsar)
+        textoSaludo = findViewById(R.id.texto_bienvenida)
 
-
+        botonPulsar.setOnClickListener({
+            Log.v("ciclo_vida", "Boton pulsado")
+            textoSaludo.setText("Primera app completa")
+        })
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.v("ciclo_vida","onStar ejecutado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("ciclo_vida","onResuma ejecutado")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("ciclo_vida","onPause ejecutado")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("ciclo_vida","onStop ejecutado")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("ciclo_vida","onDestroy ejecutado")
+    }
 
 }
