782ec0ac4826de9bf79524386f975d682d6202d2
AlexAguado24 <aaguado131@gmail.com>
2022-10-24 21:51:54 +0200

    .

--- Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
+++ Kotlin/T2/T2_Inicio/app/src/main/java/com/example/inicio_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.inicio_android
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,12 +9,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var botonPulsar: Button;
     lateinit var textoSaludo: TextView;
     lateinit var editNombre: EditText;
+    lateinit var botonPasar: Button;
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,19 +29,41 @@ class MainActivity : AppCompatActivity() {
         botonPulsar = findViewById(R.id.boton_pulsar)
         textoSaludo = findViewById(R.id.texto_bienvenida)
         editNombre = findViewById(R.id.edit_nombre)
+        botonPasar = findViewById(R.id.boton_pasar)
 
-        botonPulsar.setOnClickListener({view: View ->
-            if (!editNombre.text.isEmpty()) {
-                var texto = editNombre.text
-                textoSaludo.setText(texto)
-                editNombre.setText("")
-            } else {
-                Log.v("avisos","texto vacio")
-            }
-        })
+        botonPasar.setOnClickListener(this)
+            /*Toast.makeText(applicationContext,"Toast completada", Toast.LENGTH_SHORT).show()*/
+
+
+
+        botonPulsar.setOnClickListener(this)
     }
 
-    override fun onStart() {
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.boton_pulsar -> {
+                if (!editNombre.text.isEmpty()) {
+                    var texto = editNombre.text
+                    textoSaludo.setText(texto)
+                    editNombre.setText("")
+                } else {
+                    Log.v("avisos","texto vacio")
+                }
+            }
+            R.id.boton_pasar -> {
+                var notificacion = Snackbar.make(p0!!, "Snack completado", Snackbar.LENGTH_LONG)
+                notificacion.setAction("Seguro que quieres cerrar",{
+                    var intent: Intent = Intent(applicationContext, SecondActivity::class.java)
+                    var datos: Bundle = Bundle();
+                    datos.putString("nombre", editNombre.text.toString())
+                    intent.putExtras(datos)
+                    startActivity(intent)
+                })
+                notificacion.show()
+            }
+        }
+    }
+    /*override fun onStart() {
         super.onStart()
         Log.v("ciclo_vida","onStar ejecutado")
     }
@@ -50,6 +76,5 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.v("ciclo_vida","onPause ejecutado")
-    }
-
+    }*/
 }
