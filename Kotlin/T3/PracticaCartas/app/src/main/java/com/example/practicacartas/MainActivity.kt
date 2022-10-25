b43c4aa153600c41954c42562101e02c33bac6e3
AlexAguado24 <aaguado131@gmail.com>
2022-10-25 14:13:36 +0200

    .

---
+++ Kotlin/T3/PracticaCartas/app/src/main/java/com/example/practicacartas/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.practicacartas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var botonEmpezar: Button;
+    lateinit var textoNombre: EditText;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        instancias()
+        acciones()
+    }
+
+    private fun acciones(){
+
+    }
+
+
+    private fun instancias(){
+        botonEmpezar = this.findViewById(R.id.boton_empezar);
+        textoNombre = this.findViewById(R.id.texto_nombre);
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.boton_empezar ->{
+                if (!textoNombre.text.isEmpty()) {
+                    var intent: Intent = Intent(applicationContext,SecondActivity::class.java);
+                    var bundle: Bundle = Bundle();
+
+                } else {
+                    Toast.makeText(applicationContext,"Por favor introduce nombre",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Kotlin/T3/PracticaCartas/app/src/main/java/com/example/practicacartas/SecondActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practicacartas
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+}
