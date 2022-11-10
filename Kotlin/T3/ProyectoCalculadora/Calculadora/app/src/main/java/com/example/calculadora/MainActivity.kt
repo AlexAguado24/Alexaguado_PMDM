be5d60c93154e3998c047f4c4e216ea3ad8eceb8
AlexAguado24 <aaguado131@gmail.com>
2022-11-10 21:03:56 +0100

    .

--- Kotlin/T3/ProyectoCalculadora/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Kotlin/T3/ProyectoCalculadora/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var botonUno: Button;
+    private lateinit var botonDos: Button;
+    private var botonTres: Button? = null;
+    private var botonCuatro: Button? = null;
+    private lateinit var editNumeros: EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        instancias();
+        acciones();
+
+    }
+
+    private fun acciones() {
+        botonUno.setOnClickListener(this)
+        botonDos.setOnClickListener(this)
+        botonTres?.setOnClickListener(this)
+        botonCuatro?.setOnClickListener(this)
+    }
+
+    private fun instancias() {
+        botonUno = findViewById(R.id.boton_uno)
+        botonDos = findViewById(R.id.boton_dos)
+        botonTres = findViewById(R.id.boton_tres)
+        botonCuatro = findViewById(R.id.boton_cuatro)
+        editNumeros = findViewById(R.id.edit_numeros)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.boton_uno->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_dos->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_tres->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_cuatro->{
+                editNumeros.append((p0 as Button).text)
+            }
+        }
     }
 }
