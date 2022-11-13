98f5fc86bad7a6050a9cc121330360472f0fa393
AlexAguado24 <aaguado131@gmail.com>
2022-11-13 19:37:51 +0100

    .

--- Kotlin/T3/ProyectoCalculadora/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Kotlin/T3/ProyectoCalculadora/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -20,30 +20,84 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     private lateinit var botonComa: Button;
     private lateinit var botonIgual: Button;
     private lateinit var botonMas: Button;
-    private var botonTres: Button? = null;
-    private var botonCuatro: Button? = null;
+    private lateinit var botonMenos: Button;
+    private lateinit var botonMulti: Button;
+    private lateinit var botonDiv: Button;
+    private lateinit var botonPorcentaje: Button;
+    private lateinit var botonMasMenos: Button;
+    private lateinit var botonBorrar: Button;
+    private var botonEquiExcla: Button? = null;
+    private var botonRaiz: Button? = null;
+    private var botonPotencia: Button? = null;
+    private var botonEleEne: Button? = null;
+    private var botonE: Button? = null;
     private lateinit var editNumeros: EditText;
+    private var opUno: Int = 0;
+    private var opDos: Int = 0;
+    private var resultado: Int = 0;
+
+    private lateinit var signo: String;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         instancias();
         acciones();
-
+        supportActionBar?.hide();
     }
 
     private fun acciones() {
         botonUno.setOnClickListener(this)
         botonDos.setOnClickListener(this)
-        botonTres?.setOnClickListener(this)
-        botonCuatro?.setOnClickListener(this)
+        botonTres.setOnClickListener(this)
+        botonCuatro.setOnClickListener(this)
+        botonCinco.setOnClickListener(this)
+        botonSeis.setOnClickListener(this)
+        botonSiete.setOnClickListener(this)
+        botonOcho.setOnClickListener(this)
+        botonNueve.setOnClickListener(this)
+        botonCero.setOnClickListener(this)
+        botonComa.setOnClickListener(this)
+        botonIgual.setOnClickListener(this)
+        botonMas.setOnClickListener(this)
+        botonMenos.setOnClickListener(this)
+        botonMulti.setOnClickListener(this)
+        botonDiv.setOnClickListener(this)
+        botonPorcentaje.setOnClickListener(this)
+        botonMasMenos.setOnClickListener(this)
+        botonBorrar.setOnClickListener(this)
+        botonEquiExcla?.setOnClickListener(this)
+        botonRaiz?.setOnClickListener(this)
+        botonPotencia?.setOnClickListener(this)
+        botonEleEne?.setOnClickListener(this)
+        botonE?.setOnClickListener(this)
     }
 
     private fun instancias() {
+        editNumeros = findViewById(R.id.edit_numeros)
         botonUno = findViewById(R.id.boton_uno)
         botonDos = findViewById(R.id.boton_dos)
         botonTres = findViewById(R.id.boton_tres)
         botonCuatro = findViewById(R.id.boton_cuatro)
-        editNumeros = findViewById(R.id.edit_numeros)
+        botonCinco = findViewById(R.id.boton_cinco)
+        botonSeis = findViewById(R.id.boton_seis)
+        botonSiete = findViewById(R.id.boton_siete)
+        botonOcho = findViewById(R.id.boton_ocho)
+        botonNueve = findViewById(R.id.boton_nueve)
+        botonCero = findViewById(R.id.boton_cero)
+        botonComa = findViewById(R.id.boton_coma)
+        botonIgual = findViewById(R.id.boton_igual)
+        botonMas = findViewById(R.id.boton_mas)
+        botonMenos = findViewById(R.id.boton_menos)
+        botonMulti = findViewById(R.id.boton_multi)
+        botonDiv = findViewById(R.id.boton_div)
+        botonPorcentaje = findViewById(R.id.boton_porcentaje)
+        botonMasMenos = findViewById(R.id.boton_mas_menos)
+        botonBorrar = findViewById(R.id.boton_borrar)
+        botonEquiExcla = findViewById(R.id.boton_equis_excla)
+        botonRaiz = findViewById(R.id.boton_raiz)
+        botonPotencia = findViewById(R.id.boton_potencia)
+        botonEleEne = findViewById(R.id.boton_ele_ene)
+        botonE = findViewById(R.id.boton_e)
     }
 
     override fun onClick(p0: View?) {
@@ -59,6 +113,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
             }
             R.id.boton_cuatro->{
                 editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_cinco->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_seis->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_siete->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_ocho->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_nueve->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_cero->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_borrar->{
+                editNumeros.setText("")
+                opUno = 0
+            }
+            R.id.boton_coma->{
+                editNumeros.append((p0 as Button).text)
+            }
+            R.id.boton_mas->{
+                signo = (p0 as Button).text.toString()
+                editNumeros.setText("")
+            }
+            R.id.boton_igual->{
+                opDos = (editNumeros.text as Int)
+                when (signo) {
+                    "+"->{
+                        resultado = opUno.plus(opDos)
+                        editNumeros.setText(resultado.toString())
+                    }
+                }
             }
         }
     }
