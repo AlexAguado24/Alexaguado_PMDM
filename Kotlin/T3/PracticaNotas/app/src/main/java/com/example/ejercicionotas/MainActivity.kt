9598ca7284e09d150d1a031c18576a8c0bc54226
AlexAguado24 <aaguado131@gmail.com>
2022-11-22 11:40:55 +0100

    .

--- Kotlin/T3/PracticaNotas/app/src/main/java/com/example/ejercicionotas/MainActivity.kt
+++ Kotlin/T3/PracticaNotas/app/src/main/java/com/example/ejercicionotas/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.ejercicionotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.ejercicionotas.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var editNombre: EditText
+    lateinit var editApellido: EditText
+    lateinit var botonIniciar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        acciones();
     }
+
+    private fun acciones() {
+        binding.botonIniciar.setOnClickListener {
+            if (editNombre.text.isNotEmpty() && editApellido.text.isNotEmpty()) {
+
+            }
+        }
+    }
+
+
 }
