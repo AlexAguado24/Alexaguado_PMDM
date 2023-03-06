65cc452d4601ddd3ef4ac0afb78ad247d4ce6e65
AlexAguado24 <aaguado131@gmail.com>
2023-03-06 19:35:47 +0100

    .

--- Kotlin/T8/BBDD_Repaso/app/src/main/java/com/example/repaso_bbdd/SecondActivity.kt
+++ Kotlin/T8/BBDD_Repaso/app/src/main/java/com/example/repaso_bbdd/SecondActivity.kt
@@ -3,22 +3,30 @@ package com.example.repaso_bbdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.repaso_bbdd.databinding.ActivitySecondBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private lateinit var fDataBase : FirebaseDatabase
     private var uid : String? = null
     private var usuario : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_second)
+        fDataBase = FirebaseDatabase.getInstance("https://fir-ces-jaam-default-rtdb.firebaseio.com/");
+        setContentView(binding.root)
 
         usuario = intent.extras!!.getString("usuario")
         uid = intent.extras!!.getString("uid")
 
-        
+        binding.botonGuardar.setOnClickListener {
+            var referencia = fDataBase.getReference("Usuarios")
+            referencia.child(uid.toString()).child("nombre").setValue(usuario);
+        }
 
     }
 }
